3f100e63c4c6d7640fd9c4d038afb3e46ce25998
mryan-3 <malawaryan@gmail.com>
2023-09-19 23:24:13 +0300

    Created a basic node express server for user auth

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+
+
+app.use(express.json())
+const port = 3001
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
