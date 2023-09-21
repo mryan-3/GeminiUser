43bf505bb7f14ea327edd3cc9b77c0809a9c46a2
mryan-3 <malawaryan@gmail.com>
2023-09-21 21:33:04 +0300

    Added supabase instance

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const { createClient } = require('@supabase/supabase-js')
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 app.use(express.json())
