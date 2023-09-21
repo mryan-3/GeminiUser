785769eb50c45fa43e5577816bfe4e1f40a1798a
mryan-3 <malawaryan@gmail.com>
2023-09-22 00:24:11 +0300

    Added more endpoints to ebe tested tomorrow

--- routes/User.js
+++ routes/User.js
@@ -7,7 +7,7 @@ router.use(express.json())
 //Create a new user
 router.post('/signup', async(req, res) => {
     try{
-        const { email, body } = req.body
+        const { email, password } = req.body
         const { user, error } = await supabase.auth.signUp({
             email, password
         })
@@ -27,8 +27,42 @@ router.post('/login', async (req, res) => {
         })
         if (error) throw error
         res.json({ user })
-    }catch{
+    }catch (error){
         res.status(400).json({ error: error.message})
     }
 })
 
+// Reset password
+router.post('/reset-password', async(req, res) => {
+    try{
+        const { email } = req.body
+        const { error } = await supabase.auth.api.resetPasswordForEmail(email)
+        if (error) throw error
+        res.json({message: "Password reset link sent successfully"})
+    }catch(error){
+        res.status(400).json({ error: error.message})
+    }
+})
+
+// Update 
+router.post('/update-user', async(req, res) => {
+    try{
+        const { password } = req.body
+        const { user, error } = await supabase.auth.updateUser({
+            password
+        })
+        if(error) throw error
+        res.json({ message: "Updated password and email"})
+    }catch(error){ 
+        res.status(400).json({ error: error.message})
+    }
+})
+
+// Logout
+router.post('/logout', async(req, res) => {
+    try{
+        const { error } = await supabase.auth.signOut()
+    }catch(error){
+        res.status(400).json({ error: error.message})
+    } 
+})
