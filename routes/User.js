95154b2654bea6f48b8a8bd40411b13550550c84
mryan-3 <malawaryan@gmail.com>
2023-09-23 00:40:51 +0300

    Added google oauth configuration to User.js will check on functionality

--- routes/User.js
+++ routes/User.js
@@ -1,8 +1,43 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const router = express.Router()
 const { supabase } = require('../supabaseConfig')
 
 router.use(express.json())
+
+
+//COnfigure Google OAuth strategy using passport
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: '',
+            clientSecret: '',
+            callbackURL: '',
+        },
+        async (accessToken, refreshToken, profile, done) => {
+             // Use the profile data to sign up or sign in the user in Supabase
+            try {
+                const { email } = profile._json
+                const { user, error } = await supabase.auth.signInWithOAuth({
+                  provider: 'google',
+                })
+                if (error) throw error
+                return done(null, user)
+            } catch (error) {
+                return done(error)
+            }
+        }
+    )
+)
+
+// Initialize Passport session
+router.use(passport.initialize())
+router.use(passport.session())
+
+passport.serializeUser((user, done) => done(null, user))
+passport.deserializeUser((user, done) => done(null, user))
+
 
 //Create a new user
 router.post('/signup', async(req, res) => {
@@ -17,6 +52,24 @@ router.post('/signup', async(req, res) => {
         res.status(400).json({ error: error.message})
     }
 })
+// Google sign-in routes
+router.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+)
+  
+router.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+      successRedirect: '/success', // Redirect URL after successful sign-in
+      failureRedirect: '/error', // Redirect URL after failed sign-in
+    })
+)
+/*
+I can add a custom html tempalate for the sent email for different uses
+check out the following link: https://www.youtube.com/watch?v=mDcmmh80s9E&ab_channel=CodewithGuillaume
+also find a custom smtp provider eg. sendgrid
+*/
 
 //Login a user
 router.post('/login', async (req, res) => {
